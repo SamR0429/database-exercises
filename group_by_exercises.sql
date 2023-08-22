981b3119c37feadf902d7412601b138e8745f89d
SamR0429 <Samanatha.ramos01@gmail.com>
2023-08-22 09:27:34 -0500

    fixed problems

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -4,31 +4,35 @@ select distinct title
 from titles;
 
 
-select distinct last_name
+select last_name
 from employees
-where last_name like 'E%' and last_name like '%e'
-group by last_name;
+where last_name like '%E'
+  and last_name like 'e%'
+group by last_name
+order by last_name;
 
 
-select distinct first_name, last_name
+select first_name, last_name
 from employees
-where last_name like 'E%' and last_name like '%e'
-group by last_name;
+where last_name like '%E'
+  and last_name like 'e%'
+group by first_name ,last_name
+order by last_name;
 
 
-select distinct last_name
-from employees
-where last_name like '%q%' not like '%qu%'
-group by last_name;
-
-
-select last_name, count(last_name)
+select last_name
 from employees
 where last_name like '%q%' not like '%qu%'
 group by last_name;
 
 
-select first_name, count(*)
+select count(*) as total, last_name
 from employees
-where (first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya')
-   or gender = 'M' or gender = 'F';
+where last_name like '%q%' not like '%qu%'
+group by last_name;
+
+
+select count(*) as total , gender
+from employees
+where first_name in ('Irena','Maya', 'Vidya')
+group by gender;
