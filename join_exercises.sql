c379f0e50ed9b360a4cdbd8ac909c5af16b99ae9
SamR0429 <Samanatha.ramos01@gmail.com>
2023-08-23 10:25:53 -0500

    finished exercise

--- join_exercises.sql
+++ join_exercises.sql
@@ -32,11 +32,28 @@ where dm.to_date = '9999-01-01'
 order by d.dept_name;
 
 
-select t.title as Title, COUNT(*) AS Total
+select t.title as Title, COUNT(t.emp_no) AS Total
 from titles t
          join dept_emp de
               on t.emp_no = de.emp_no
          join departments d
-              on de.dept_no = d.dept_no
+              on d.dept_no = de.dept_no
+where d.dept_name = 'Customer Service'
+  and t.to_date = '9999-01-01'
+  and de.to_date = '9999-01-01'
+group by t.title;
+
+
+select d.dept_name as "Department Name" , CONCAT(e.first_name, ' ', e.last_name) as "Department Manager" , s.salary as Salaray
+from departments d
+         join dept_manager dm
+              on d.dept_no = dm.dept_no
+         join employees e
+              on dm.emp_no = e.emp_no
+         join salaries s
+              on e.emp_no = s.emp_no
+where dm.to_date = '9999-01-01'
+and s.to_date = '9999-01-01'
+order by s.salary desc;
 
 
